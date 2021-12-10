6e159bfd92f7f6f0839db4690c313f4eb81a39af
gawiese <89753989+gawiese@users.noreply.github.com>
2021-12-10 07:27:17 -0700

    Add files via upload
    Creates a basic class, main method and prints Welcome to Java!

---
+++ Main.java
@@ -0,0 +1,14 @@
+/// Garrett Wiese
+
+/// Class : CSC160179FA21
+/// Summary of purpose for program : establishes the class on Line 8,
+/// Creates main method on line 10.
+/// Line 12 prints the string "Welcome to Java!"
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java!");
+    }
+}
